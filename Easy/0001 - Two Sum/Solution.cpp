3d19b35c1fc3c0470301fc87a14ad9079234d354
Abdullah Zia <63127711+abdullahx404@users.noreply.github.com>
2026-07-01 06:05:48 +0500

    Add Two Sum (C++)

--- Easy/0001 - Two Sum/Solution.cpp
+++ Easy/0001 - Two Sum/Solution.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> seen;
+        unordered_map<int, int> seens;
         
         for (int i = 0; i < nums.size(); ++i) {
             int complement = target - nums[i];
             
-            if (seen.find(complement) != seen.end()) {
-                return {seen[complement], i};
+            if (seens.find(complement) != seens.end()) {
+                return {seens[complement], i};
             }
             
-            seen[nums[i]] = i;
+            seens[nums[i]] = i;
         }
         
         return {};
